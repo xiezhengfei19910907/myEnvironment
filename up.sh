91796d9ba0327c9b70f2a0d9b234b4088bd996d7
zfxie <zfxie@i9i8.com>
2016-06-16 18:46:05 +0800

    refactor nginx install issue

--- up.sh
+++ up.sh
@@ -6,7 +6,7 @@ sudo apt-get install -y build-essential
 sudo apt-get install -y memcached
 sudo apt-get install -y node.js
 sudo apt-get install -y npm
-sudo apt-get install -y php5-cli php5-cgi php5-fpm php5-mcrypt php5-mysql php5-memcached  php5-curl php5-gmp php5-xcache
+sudo apt-get install -y php5-cli php5-cgi php5-fpm php5-mcrypt php5-mysql php5-memcached php5-curl php5-gmp php5-xcache
 
 sudo ln -s -f /usr/bin/nodejs /usr/bin/node
 sudo bash -c "curl -sS https://getcomposer.org/installer | php -- --install-dir=/usr/bin"
@@ -16,62 +16,11 @@ sudo apt-get install -y libxml2-dev libbz2-dev libgmp-dev libmcrypt-dev libreadl
 sudo apt-get install -y pkg-config
 sudo apt-get install -y libpcre3 libpcre3-dev
 sudo ln -s -f /usr/include/x86_64-linux-gnu/gmp.h /usr/include/gmp.h
-sudo apt-get install -y  libgd2-xpm-dev libgeoip-dev libgoogle-perftools-dev libperl-dev
+sudo apt-get install -y libgd2-xpm-dev libgeoip-dev libgoogle-perftools-dev libperl-dev
 
-cd /tmp
-wget http://nginx.org/download/nginx-1.8.0.tar.gz
-tar xzvf nginx-1.8.0.tar.gz
-cd nginx-1.8.0
-./configure \
-    --prefix=/usr/share/nginx                                                                                                                   \
-    --sbin-path=/usr/sbin/nginx                                                                                                                 \
-    --conf-path=/etc/nginx/nginx.conf                                                                                                           \
-    --error-log-path=/var/log/nginx/error.log                                                                                                   \
-    --http-log-path=/var/log/nginx/access.log                                                                                                   \
-    --http-client-body-temp-path=/var/lib/nginx/tmp/client_body                                                                                 \
-    --http-proxy-temp-path=/var/lib/nginx/tmp/proxy                                                                                             \
-    --http-fastcgi-temp-path=/var/lib/nginx/tmp/fastcgi                                                                                         \
-    --http-uwsgi-temp-path=/var/lib/nginx/tmp/uwsgi                                                                                             \
-    --http-scgi-temp-path=/var/lib/nginx/tmp/scgi                                                                                               \
-    --pid-path=/var/run/nginx.pid                                                                                                               \
-    --lock-path=/var/lock/subsys/nginx                                                                                                          \
-    --user=nginx                                                                                                                                \
-    --group=nginx                                                                                                                               \
-    --with-file-aio                                                                                                                             \
-    --with-ipv6                                                                                                                                 \
-    --with-http_ssl_module                                                                                                                      \
-    --with-http_spdy_module                                                                                                                     \
-    --with-http_realip_module                                                                                                                   \
-    --with-http_addition_module                                                                                                                 \
-    --with-http_xslt_module                                                                                                                     \
-    --with-http_image_filter_module                                                                                                             \
-    --with-http_geoip_module                                                                                                                    \
-    --with-http_sub_module                                                                                                                      \
-    --with-http_dav_module                                                                                                                      \
-    --with-http_flv_module                                                                                                                      \
-    --with-http_mp4_module                                                                                                                      \
-    --with-http_gunzip_module                                                                                                                   \
-    --with-http_gzip_static_module                                                                                                              \
-    --with-http_random_index_module                                                                                                             \
-    --with-http_secure_link_module                                                                                                              \
-    --with-http_degradation_module                                                                                                              \
-    --with-http_stub_status_module                                                                                                              \
-    --with-http_perl_module                                                                                                                     \
-    --with-mail                                                                                                                                 \
-    --with-mail_ssl_module                                                                                                                      \
-    --with-pcre                                                                                                                                 \
-    --with-google_perftools_module                                                                                                              \
-    --with-debug                                                                                                                                \
-    --with-cc-opt='-O2 -g -pipe -Wall -Wp,-D_FORTIFY_SOURCE=2 -fexceptions -fstack-protector --param=ssp-buffer-size=4 -m64 -mtune=generic'     \
-    --with-ld-opt=' -Wl,-E'
-
-sudo make && make install
-sudo mkdir -p /var/lib/nginx/tmp/client_body
+sudo apt-get install nginx
 sudo mkdir -p /var/nginxproxycache/client_temp
-
 sudo cp -r /var/www/nginx_config/* /etc/nginx/
-sudo cp /var/www/nginx /etc/init.d
-sudo update-rc.d nginx defaults
 
 sudo groupadd -r nginx
 sudo useradd -r -g nginx -s /bin/false -M nginx
@@ -109,8 +58,8 @@ sudo php bin/lang_pack.php -d"JJsHouse" -l"en de es fr se no it pt da fi ru nl j
 
 sudo apt-get install -y dos2unix
 sudo apt-get install -y unix2dos
-sudo dos2unix /etc/init.d/nginx
-sudo systemctl daemon-reload
+#sudo dos2unix /etc/init.d/nginx
+#sudo systemctl daemon-reload
 
 sudo service nginx restart
 sudo service php5-fpm restart
